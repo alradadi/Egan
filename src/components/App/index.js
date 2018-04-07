6b9e2c60f3a496d88bd134b989ec5ac8f9a5cf5d
Abdullah <hrfssa@gmail.com>
2018-04-07 00:33:13 -0700

    some css

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
-  Route,
+    BrowserRouter as Router,
+    Route,
 } from 'react-router-dom';
-
+import CssBaseline from 'material-ui/CssBaseline';
+import {withStyles} from 'material-ui/styles';
 import Navigation from '../Navigation';
 import LandingPage from '../Landing';
 import SignUpPage from '../SignUp';
@@ -14,26 +15,45 @@ import AccountPage from '../Account';
 import withAuthentication from '../Session/withAuthentication';
 import * as routes from '../../constants/routes';
 
-import './index.css';
 
-const App = () =>
-  <Router>
-    <div className="app">
-      <Navigation />
+const styles = {
+    '@global': {
+        'html, body': {
+            height: '100%',
+            backgroundColor: '#fff',
+        },
+    },
+    app: {
+        height: '100%',
+        backgroundColor: '#fafafa',
+        maxWidth: 600,
+        margin: '0 auto',
+        overflow: 'auto',
+    },
+};
 
-      <hr/>
 
-      <Route exact path={routes.LANDING} component={() => <LandingPage />} />
-      <Route exact path={routes.SIGN_UP} component={() => <SignUpPage />} />
-      <Route exact path={routes.SIGN_IN} component={() => <SignInPage />} />
-      <Route exact path={routes.PASSWORD_FORGET} component={() => <PasswordForgetPage />} />
-      <Route exact path={routes.HOME} component={() => <HomePage />} />
-      <Route exact path={routes.ACCOUNT} component={() => <AccountPage />} />
+const App = (props) =>
+    <Router>
+        <div className={props.classes.app}>
+            <CssBaseline/>
+            <Navigation/>
 
-      <hr/>
+            <hr/>
 
-      <span>Found in <a href="https://roadtoreact.com/course-details?courseId=TAMING_THE_STATE">Taming the State in React</a></span> | <span>Star the <a href="https://github.com/rwieruch/react-firebase-authentication">Repository</a></span> | <span>Receive a <a href="https://www.getrevue.co/profile/rwieruch">Developer's Newsletter</a></span>
-    </div>
-  </Router>
+            <Route exact path={routes.LANDING} component={() => <LandingPage/>}/>
+            <Route exact path={routes.SIGN_UP} component={() => <SignUpPage/>}/>
+            <Route exact path={routes.SIGN_IN} component={() => <SignInPage/>}/>
+            <Route exact path={routes.PASSWORD_FORGET} component={() => <PasswordForgetPage/>}/>
+            <Route exact path={routes.HOME} component={() => <HomePage/>}/>
+            <Route exact path={routes.ACCOUNT} component={() => <AccountPage/>}/>
 
-export default withAuthentication(App);
+            <hr/>
+
+            <span>Found in <a href="https://roadtoreact.com/course-details?courseId=TAMING_THE_STATE">Taming the State in React</a></span> | <span>Star the <a
+            href="https://github.com/rwieruch/react-firebase-authentication">Repository</a></span> | <span>Receive a <a
+            href="https://www.getrevue.co/profile/rwieruch">Developer's Newsletter</a></span>
+        </div>
+    </Router>
+
+export default withAuthentication(withStyles(styles)(App));
