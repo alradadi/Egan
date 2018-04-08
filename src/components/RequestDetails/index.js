9e884f79a7fcc191cf45a386f72183b40b11c851
Abdullah <hrfssa@gmail.com>
2018-04-08 10:36:36 -0700

    msg

--- src/components/RequestDetails/index.js
+++ src/components/RequestDetails/index.js
@@ -2,185 +2,255 @@ import React, {Component} from 'react';
 import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
 import {withStyles} from 'material-ui/styles';
+import IconButton from 'material-ui/IconButton';
+import ModeEdit from 'material-ui-icons/ModeEdit';
 import Loader from '../Loader';
 import {auth, db} from '../../firebase';
 import WithAuthorization from '../Session/withAuthorization';
+import grey from "material-ui/colors/grey";
 
 
 const styles = theme => ({
     container: {
         display: 'flex',
         flexDirection: 'column',
-        marginLeft: 25,
-        marginRight: 25,
+        height: '100%',
+        position: 'relative',
+        padding: 15,
     },
-    textField: {
+    row: {
+        marginBottom: 12,
     },
-    button: {
-        margin: theme.spacing.unit,
+    rowText: {
+        color: grey[500],
     },
-    h2: {
-        margin: theme.spacing.unit,
+    editButton: {
+        position: 'absolute',
+        right: 0,
+        top: 0,
+    },
+    buttonsContainer: {
+        marginTop: 'auto',
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    saveButton: {
+        marginLeft: 10,
+    },
+    cancelButton: {},
+    title: {
+        fontSize: '1.5rem',
+        marginBottom: 15,
+        marginRight: 26,
+    },
+    closeRequestBtn:{
+        marginRight: 'auto',
     }
-});
-
-const updateByPropertyName = (propertyName, value) => () => ({
-    [propertyName]: value,
 });
 
 
 class RequestDetailsView extends Component {
     constructor(props) {
         super(props);
-        this.onClickSave = this.onClickSave.bind(this);
-        this.onClickEdit = this.onClickEdit.bind(this);
-        this.onClickClose = this.onClickClose.bind(this);
-        this.onClickCreate = this.onClickCreate.bind(this);
         this.state = {
-            id: this.props.match.params.requestId,
-            site: 'test site',
-            requester: '',
-            time: '',
-            details: '',
-            disabledForm: true,
-            displayDelete: false,
-            displaySave: false,
-            displayEdit: false,
-            error: null,
+            // id: this.props.match.params.requestId,
+            // site: 'test site',
+            // requester: '',
+            // time: '',
+            // details: '',
+            // disabledForm: true,
+            // displayDelete: false,
+            // displaySave: false,
+            // displayEdit: false,
+            // error: null,
+            // fetching: true,
+            request: {},
+            editing: false,
             fetching: true,
         };
+        this.toggleEditing = this.toggleEditing.bind(this);
+        this.cancelEditing = this.cancelEditing.bind(this);
+        this.saveForm = this.saveForm.bind(this);
+        this.closeRequest = this.closeRequest.bind(this);
     }
 
     componentDidMount() {
         this.fetchRequest();
     }
 
-    fetchRequest() {
-        let incident = db.getRequest(this.state.id).then(snapshot => {
-            let v = snapshot.val();
-            console.log(v);
-            if (v != null)
-                this.setState(() => (
-                    {   site: v.site,
-                        requester: v.requester,
-                        time: v.time,
-                        details: v.details,
-                        status: v.status,
-                        fetching: false,
-                    }));
-        });
-    }
-
-    onClickSave() {
+    toggleEditing() {
         this.setState({
-            disabledForm: true,
+            editing: !this.state.editing,
         });
-        const d = new Date();
-        const newTime = d.toISOString().split('.')[0];
-        console.log(newTime);
-        const { site, details } = this.state;
-        let updates = {site: site, details: details, time: newTime};
-        db.updateRequest(this.state.id, updates);
-        this.fetchRequest();
     }
 
-    onClickEdit() {
+    editState(field, newValue) {
+        const request = {...this.state.request, [field]: newValue};
+        this.setState({request});
+    }
+
+    onInputChange(field) {
+        return event => {
+            return this.editState(field, event.target.value);
+        };
+    }
+
+    cancelEditing() {
+        this.setState({editing: false, request: this.seedRequest});
+    }
+
+    saveForm() {
+        const id = this.props.match.params.requestId;
+        db.updateRequest(id, this.state.request);
         this.setState({
-            disabledForm: !this.state.disabledForm,
+            editing: false,
         });
     }
 
-    onClickClose() {
+    closeRequest(){
         this.setState({
-            disabledForm: true,
+            editing: false,
         });
-        let updates = {status: 'closed'};
-        db.updateRequest(this.state.id, updates);
-        this.fetchRequest();
+        const id = this.props.match.params.requestId;
+        db.updateRequest(id, {status: 'closed'});
+        this.props.history.push('/requests');
     }
 
-    onClickCreate() {
-        db.doCreateIncident(this.state.incident.title, this.state.incident.time, 'joey', 'testSite', this.state.incident.details);
+    async fetchRequest() {
+        const id = this.props.match.params.requestId;
+        const snapshot = await db.getRequest(id);
+        const request = snapshot.val();
+        this.setState({
+            request,
+            fetching: false
+        });
+        this.seedRequest = request;
     }
+
 
     render() {
-        const { classes } = this.props;
-        const {
-            id,
-            status,
-            site,
-            requester,
-            time,
-            details,
-            displayEdit,
-            disabledForm,
-            error,
-        } = this.state;
-
-        if(this.state.fetching){
-            return <Loader/>
+        const {props, state} = this;
+        const {classes} = props;
+        const {request} = state;
+        if (this.state.fetching) {
+            return <Loader/>;
         }
-        return (
-            <div className={classes.container}>
-                <h1>{`Order # ${id} -- ${status}`}</h1>
-                <TextField
-                    className={classes.textField}
-                    label='Site Name'
-                    value={site}
-                    onChange={event => this.setState(updateByPropertyName('site', event.target.value))}
-                    type="text"
-                    disabled
-                />
-                <TextField
-                    className={classes.textField}
-                    label='Date & Time'
-                    value={time}
-                    onChange={event => this.setState(updateByPropertyName('time', event.target.value))}
-                    type="datetime-local"
-                    defaultValue="2017-05-24T10:30"
-                    disabled
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                />
-
-                <TextField
-                    className={classes.textField}
-                    label='Requester'
-                    value={requester}
-                    onChange={event => this.setState(updateByPropertyName('details', event.target.value))}
-                    type="text"
-                    disabled
-                />
-                <TextField
-                    multiline
-                    rowsMax="4"
-                    className={classes.textField}
-                    label='Request Details'
-                    value={details}
-                    onChange={event => this.setState(updateByPropertyName('details', event.target.value))}
-                    type="text"
-                    disabled={disabledForm}
-                />
-                <div style={{display: 'flex', justifyContent: 'center'}}>
-                    <Button variant="raised" style={{ backgroundColor: 'orange'}} className={classes.button}
-                            onClick={this.onClickEdit}>Edit</Button>
-                    <Button variant="raised" color="primary" className={classes.button} disabled={disabledForm}
-                            onClick={this.onClickSave}>
-                        Save
-                    </Button>
-                    <Button variant="raised" color="secondary" className={classes.button}
-                            onClick={this.onClickClose}>
-                        Close
-                    </Button>
+        if (state.editing) {
+            return (
+                <div className={classes.container}>
+                    <div className={classes.title}>Supplies Order</div>
+                    <div className={classes.row}>
+                        <div>
+                            Site Name
+                        </div>
+                        <div className={classes.rowText}>
+                            {request.site || ''}
+                        </div>
+                    </div>
+                    <div className={classes.row}>
+                        <div>
+                            Requester
+                        </div>
+                        <div className={classes.rowText}>
+                            {request.requester || ''}
+                        </div>
+                    </div>
+                    <div className={classes.row}>
+                        <div>
+                            Date & Time
+                        </div>
+                        <div className={classes.rowText}>
+                            {(new Date(request.time)).toLocaleString() || ''}
+                        </div>
+                    </div>
+                    <div className={classes.row}>
+                        <div>
+                            request Details
+                        </div>
+                        <TextField
+                            fullWidth
+                            type="text"
+                            multiline
+                            value={request.details || ''}
+                            onChange={this.onInputChange('details')}
+                        />
+                    </div>
+                    <div className={classes.buttonsContainer}>
+                        <Button
+                            className={classes.closeRequestBtn}
+                            onClick={this.closeRequest}
+                            color="secondary"
+                            variant="raised"
+                            size="small"
+                        >
+                            Close Request
+                        </Button>
+                        <Button
+                            className={classes.cancelButton}
+                            onClick={this.cancelEditing}
+                            color="secondary"
+                            variant="raised"
+                            size="small"
+                        >
+                            Cancel
+                        </Button>
+                        <Button
+                            className={classes.saveButton}
+                            color="primary"
+                            variant="raised"
+                            size="small"
+                            onClick={this.saveForm}
+                        >
+                            Save
+                        </Button>
+                    </div>
                 </div>
-            </div>
-
-
-            // {error && <p>{error.message}</p>}
-        );
+            );
+        } else {
+            return (
+                <div className={classes.container}>
+                    <IconButton className={classes.editButton} onClick={this.toggleEditing}>
+                        <ModeEdit/>
+                    </IconButton>
+                    <div className={classes.title}>Supplies Order</div>
+                    <div className={classes.row}>
+                        <div>
+                            Site Name
+                        </div>
+                        <div className={classes.rowText}>
+                            {request.site || ''}
+                        </div>
+                    </div>
+                    <div className={classes.row}>
+                        <div>
+                            Requester
+                        </div>
+                        <div className={classes.rowText}>
+                            {request.requester || ''}
+                        </div>
+                    </div>
+                    <div className={classes.row}>
+                        <div>
+                            Date & Time
+                        </div>
+                        <div className={classes.rowText}>
+                            {(new Date(request.time)).toLocaleString() || ''}
+                        </div>
+                    </div>
+                    <div className={classes.row}>
+                        <div>
+                            request Details
+                        </div>
+                        <div className={classes.rowText}>
+                            {request.details}
+                        </div>
+                    </div>
+                </div>
+            );
+        }
     }
 }
+
 const authCondition = (authUser) => !!authUser;
 
 export default WithAuthorization(authCondition)(withStyles(styles)(RequestDetailsView));
